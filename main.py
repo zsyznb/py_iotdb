d5ef578035002ef581acaefe7dc774d3ada1d23b
zsyznb <sktotto970@gmail.com>
2023-05-08 09:04:14 +0800

    create seesion

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from iotdb.Session import Session
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ip = "127.0.0.1"
+port_ = "6667"
+username_ = "root"
+password_ = "root"
+session = Session(ip, port_, username_, password_, fetch_size=1024, zone_id="UTC+8")
+session.open(False)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# set and delete databases
+session.set_storage_group("root.sg_test_01")
