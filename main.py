f6fdee8a97731e2630867cc12692ab92366b6e47
zsyznb <sktotto970@gmail.com>
2023-05-09 22:15:28 +0800

    insert a record

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from iotdb.Session import Session
 from iotdb.utils.IoTDBConstants import TSDataType, TSEncoding, Compressor
+import datetime
+import time
+
+
+def TimetoTimeSpan(timeInfo):
+    timeArray = time.strptime(timeInfo, "%Y-%m-%d %H:%M:%s")
+    timeSpan = int(time.mktime(timeArray))
+    return timeSpan
+
 
 ip = "127.0.0.1"
 port_ = "6667"
@@ -14,3 +23,5 @@ session.set_storage_group("root.sg_test_01")
 session.create_time_series(
     "root.sg_test_01.d_01.s_01", TSDataType.BOOLEAN, TSEncoding.PLAIN, Compressor.SNAPPY
 )
+
+session.insert_record("root.sg_test_01.d_01", TimetoTimeSpan("2023-05-09 22:00:00"), "s_01", TSDataType.BOOLEAN, False)
