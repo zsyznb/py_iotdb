d06a054861fb6cb861a44b618f42293b56313955
zsyznb <sktotto970@gmail.com>
2023-05-10 10:01:47 +0800

    create aligned timeseries and insert tablet

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ from iotdb.Session import Session
 from iotdb.utils.IoTDBConstants import TSDataType, TSEncoding, Compressor
 import datetime
 import time
-
-
-def TimetoTimeSpan(timeInfo):
-    timeArray = time.strptime(timeInfo, "%Y-%m-%d %H:%M:%S")
-    timeSpan = int(time.mktime(timeArray))
-    return timeSpan
-
+import random
+from iotdb.utils.Tablet import Tablet
 
 ip = "127.0.0.1"
 port_ = "6667"
@@ -17,15 +12,40 @@ password_ = "root"
 session = Session(ip, port_, username_, password_, fetch_size=1024, zone_id="UTC+8")
 session.open(False)
 
-# set and delete databases
-session.set_storage_group("root.sg_test_01")
+# create database
+session.set_storage_group("root.test")
 
-session.create_time_series(
-    "root.sg_test_01.d_01.s_01", TSDataType.BOOLEAN, TSEncoding.PLAIN, Compressor.SNAPPY
+# set aligned timeseries
+measurement_list = [
+    "sensor_1",
+    "sensor_2",
+    "sensor_3",
+]
+datatype_list = [
+    TSDataType.FLOAT,
+    TSDataType.FLOAT,
+    TSDataType.FLOAT,
+]
+encoding_list = [
+    TSEncoding.PLAIN for _ in range(len(datatype_list))
+]
+compressor_list = [
+    Compressor.SNAPPY for _ in range(len(datatype_list))
+]
+session.create_aligned_time_series("root.test.vehicle_01", measurement_list, datatype_list, encoding_list, compressor_list)
+
+# insert a tablet
+value_list = []
+timestamp_list = []
+for i in range(100):
+    list_ = []
+    timestamp_list.append(time.time()-i)
+    for _ in range(len(datatype_list)):
+        list_.append(random.uniform(50.0, 100.0))
+    value_list.append(list_)
+tablet = Tablet(
+    "root.test.vehicle_01",measurement_list,datatype_list,value_list,timestamp_list
 )
-data_types = [TSDataType.BOOLEAN]
-l1 = ["s_01"]
-l2 = [False]
+session.insert_tablet(tablet)
 
-session.insert_record("root.sg_test_01.d_01", TimetoTimeSpan("2023-05-09 22:00:00"), l1,  data_types, l2)
-session.execute_query_statement("select s_01 from root.sg_test_01.d_01")
+
